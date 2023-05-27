0a2a727f2935c33ad24c27216051bcd520e1235b
JuanDiegoAlaPer <juandiegoap2000@gmail.com>
2023-05-27 16:04:31 -0500

    Sent messages page made

--- proy_client/src/config/router.js
+++ proy_client/src/config/router.js
@@ -7,17 +7,19 @@ import { Register }from "../pages/web/Register/register";
 import { NotFound } from "../pages/web/NotFound/NotFound";
 import { LayoutLogin } from "../Layouts/Login/LayoutLogin";
 import { redactar } from "../pages/admin/Write/redactar";
+import { Sent } from "../pages/admin/Sent/Sent";
 
 /* Components > Layouts > Pages > routes */
 const AdminRoutes = [
   { path: "/admin", component: Admin, layout: LayoutGeneral },
   { path: "/admin/users", component: Usuarios, layout: LayoutGeneral },
   { path: "/login", component: Singin , layout: LayoutLogin },
+  { path: "/admin/mensajes/redactar", component: redactar, layout: LayoutGeneral },
+  { path: "/admin/mensajes/enviados", component: Sent, layout: LayoutGeneral },
 ];
 
 const GeneralRoutes = [
-    { path: "/users", component: Register, layout: LayoutLogin },
-    { path: "/admin/mensajes/redactar", component: redactar, layout: LayoutGeneral },
+    { path: "/users", component: Register, layout: LayoutLogin },    
     { path: "/", component: Home, layout: LayoutLogin },
     { path: "*", component: NotFound, layout: LayoutGeneral },
 ];

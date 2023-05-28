b217d814bc0837e588a53ef3d5b7ea69d517f015
LuisaBetancur72 <118400684+LuisaBetancur72@users.noreply.github.com>
2023-05-28 18:08:33 -0500

    Juand (#19)
    * Create user as admin pade made
    * router before changes
    
    ---------
    
    Co-authored-by: JuanDiegoAlaPer <juandiegoap2000@gmail.com>

--- proy_client/src/pages/admin/Usuarios/Usuarios.js
+++ proy_client/src/pages/admin/Usuarios/Usuarios.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "./Usuarios.scss";
+import { Button } from "antd";
 
 export const Usuarios = () => {
   const [usuarios, setUsuarios] = useState([]);
